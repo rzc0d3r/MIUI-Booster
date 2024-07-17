04aa01310d457144f093325a7719402a2c86bbef
Artem <106631540+rzc0d3r@users.noreply.github.com>
2024-07-17 19:54:49 +0300

    Update post-fs-data.sh

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -3,3 +3,8 @@ MODDIR=${0%/*}
 rm -rf /data/app/GlobalFashiongallery
 rm -rf /data/app/MiCreditInStub
 rm -rf /data/app/wps_lite
+
+settings put system low_battery_dialog_disabled 1
+settings put global window_animation_scale 0.9
+settings put global animator_duration_scale 0.9
+settings put global transition_animation_scale 0.9
