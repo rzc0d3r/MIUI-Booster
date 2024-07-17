32a0656ff391f61e5be9186852889ddc82e64d91
Artem <106631540+rzc0d3r@users.noreply.github.com>
2024-07-17 19:56:18 +0300

    Update post-fs-data.sh

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -5,6 +5,6 @@ rm -rf /data/app/MiCreditInStub
 rm -rf /data/app/wps_lite
 
 settings put system low_battery_dialog_disabled 1
-settings put global window_animation_scale 0.9
-settings put global animator_duration_scale 0.9
-settings put global transition_animation_scale 0.9
+settings put global window_animation_scale 0.8
+settings put global animator_duration_scale 0.8
+settings put global transition_animation_scale 0.8
